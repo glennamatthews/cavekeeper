72e5baf021d0e63cf2bf66f17f952eb2b5ecda5f
Glenn Matthews <glenn.a.matthews@gmail.com>
2026-06-12 13:25:46 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,27 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    dedupe: ['react', 'react-dom', '@supabase/supabase-js'],
+  },
   optimizeDeps: {
     include: ['@supabase/supabase-js', 'xlsx'],
+    esbuildOptions: {
+      target: 'es2020',
+    },
+  },
+  build: {
+    target: 'es2020',
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('@supabase')) return 'supabase';
+          if (id.includes('xlsx')) return 'xlsx';
+        }
+      }
+    }
   },
 })
