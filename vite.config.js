4ecc4e81bb4b603d0e77c941cc3135512e8130a7
Glenn Matthews <glenn.a.matthews@gmail.com>
2026-06-12 13:31:40 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,8 @@ export default defineConfig({
   },
   build: {
     target: 'es2020',
+    sourcemap: 'inline',
+    minify: false,
     commonjsOptions: { transformMixedEsModules: true },
     rollupOptions: {
       output: {
